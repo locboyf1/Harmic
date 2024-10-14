a3d96ef6b1ff6320e8965fb8575b4a5010497164
locboyf1 <locboyf1@gmail.com>
2024-10-15 01:22:44 +0700

    TH7 UDW

---
+++ Utilities/Function.cs
@@ -0,0 +1,10 @@
+﻿namespace Harmic.Utilities
+{
+    public class Function
+    {
+        public static string TitleToAlias(string title)
+        {
+            return SlugGenerator.SlugGenerator.GenerateSlug(title);
+        }
+    }
+}
--- obj/Debug/net8.0/Harmic.AssemblyInfo.cs
+++ obj/Debug/net8.0/Harmic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44dde9a246a4aa5afdc5ad71f420ed373bc54ff5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e6262af80b81290a47a93d173d9a22f7a63c53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
