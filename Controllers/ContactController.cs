db0d1e3a63c435672a76b91b5aafa80c6839f8ec
locboyf1 <locboyf1@gmail.com>
2024-11-27 12:02:55 +0700

    TH11 TH12

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Harmic.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,19 +11,10 @@ namespace Harmic.Controllers
         {
             _context = context;
         }
-=======
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace CarRental.Controllers
-{
-    public class ContactController : Controller
-    {
->>>>>>> e65970a8c5da85161122fc0bdbc0681f49337f2d
         public IActionResult Index()
         {
             return View();
         }
-<<<<<<< HEAD
 
         [HttpPost]
         public IActionResult Create(string name, string phone, string email, string message)
@@ -46,7 +36,5 @@ namespace CarRental.Controllers
                 return Json(new { status = false });
             }
         }
-=======
->>>>>>> e65970a8c5da85161122fc0bdbc0681f49337f2d
     }
 }
